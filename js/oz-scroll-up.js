87ca3cc88ccb37534ad8cdd34b3d15f303d19f9f
Osama <osama.elzero@gmail.com>
2014-11-03 10:29:48 +0300

    Fixed Js Errors And Added New Style
    Fixed Js Errors And Added New Style

--- js/oz-scroll-up.js
+++ js/oz-scroll-up.js
@@ -3,13 +3,20 @@
 	Created By : Osama Elzero
 	Website : elzero.info
 */
+/*global $, jQuery, alert*/
 
-$(function() {
+
+$(function () {
+    "use strict";
 	var $ele = $('#oz-scroll');
-    $(window).scroll(function() {
-        $(this).scrollTop() >= 200 ? $ele.show(10).animate({ right : '15px' }, 10) : $ele.animate({ right : '-80px' }, 10);
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 200) {
+            $ele.show(10).animate({right: '15px'}, 10);
+        } else {
+            $ele.animate({right: '-80px'}, 10);
+        }
     });
-    $ele.click(function(e) {
+    $ele.click(function (e) {
         e.preventDefault();
         $('html,body').animate({
             scrollTop: 0
